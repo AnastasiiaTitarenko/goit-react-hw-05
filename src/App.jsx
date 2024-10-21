3c5ca100fb976b805df9d062d47f4fc511278d99
AnastasiiaTitarenko <151573636+AnastasiiaTitarenko@users.noreply.github.com>
2024-10-21 17:04:14 +0300

    add MovieCast

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import { Suspense,lazy } from 'react';
+import Navigation from "./components/Navigation/Navigation";
+
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage"));
+const MovieReviews = lazy(() => import("./components/MovieReviews/MovieReviews"));
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
+
 
 
 
