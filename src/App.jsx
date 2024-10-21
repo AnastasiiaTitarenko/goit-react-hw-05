6420230c376a688ce13bb6f0a0677ce0d3d0b1a8
AnastasiiaTitarenko <151573636+AnastasiiaTitarenko@users.noreply.github.com>
2024-10-21 18:15:17 +0300

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import { Suspense,lazy } from 'react';
+import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Navigation from "./components/Navigation/Navigation";
-
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
-const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage"));
-const MovieReviews = lazy(() => import("./components/MovieReviews/MovieReviews"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MovieReviews = lazy(() =>
+  import("./components/MovieReviews/MovieReviews")
+);
 
 const App = () => {
   return (
@@ -19,18 +21,15 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </>
   );
-}
+};
 
-
-
-
-
-export default App
+export default App;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,25 +1,25 @@
-import css from "./MovieList.module.css";
 import { Link, useLocation } from "react-router-dom";
+import css from "./MovieList.module.css";
 
 export default function MovieList({ movies }) {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <ul className={css.MovieList}>
-            {movies.map((movie) => (
-                <li key={movie.id} className={css.movieListItem}>
-                    <Link to={{
-                        pathname: `/movies/${movie.id}`,
-                        state: { from: location },
-                    }}
-                        className={css.movieLink}
-                    >
-                        <h3 className={css.movie.Title}>{movie.title}</h3>
-                        <p className={css.movieOverview}>{movie.overview}</p>
-                    </Link>
-                </li>
-            ))}
-
-        </ul>
-    );
+  return (
+    <ul className={css.movieList}>
+      {movies.map((movie) => (
+        <li key={movie.id} className={css.movieListItem}>
+          <Link
+            to={{
+              pathname: `/movies/${movie.id}`,
+              state: { from: location },
+            }}
+            className={css.movieLink}
+          >
+            <h3 className={css.movieTitle}>{movie.title}</h3>
+            <p className={css.movieOverview}>{movie.overview}</p>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
